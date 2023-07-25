77aa592a2d3eee4945283f27cc3c6ecfa0436d08
ignaciocuyun <ignacio.cuyun@telus.com>
2023-07-25 13:17:53 -0600

    use scroll y instead of page offset

--- src/hooks/useFabButton.ts
+++ src/hooks/useFabButton.ts
@@ -8,7 +8,7 @@ export const useFabButton = (): FabButton => {
     const [showButton, setShowButton] = useState<boolean>(false);
 
     function handleScroll(): void {
-        if (window.pageYOffset > 0) {
+        if (window.scrollY > 0) {
             setShowButton(true);
         } else {
             setShowButton(false);
